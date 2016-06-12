9b0e63e9e1b433a9dc24d48e6e32a952a2c8498b
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2016-06-13 01:27:39 +0300

    I added support for writting js unittests with jasmine-node.

---
+++ tests/unittests/core/js/networking/http_spec.js
@@ -0,0 +1,8 @@
+
+const http = require("js/networking/http");
+
+describe("Test suites for unit testing http.js implementation.", () => {
+    it("http get for string content works as expected.", () => {
+        expect(true).toBe(false);
+    });
+});
