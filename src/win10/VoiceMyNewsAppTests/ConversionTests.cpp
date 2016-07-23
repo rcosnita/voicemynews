27921d54d4e64088f2c897af241efbde2897892f
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2016-07-23 17:39:38 +0300

    I added the unit tests for converting maps between std and winrt.

--- src/win10/VoiceMyNewsAppTests/ConversionTests.cpp
+++ src/win10/VoiceMyNewsAppTests/ConversionTests.cpp
@@ -4,7 +4,9 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace voicemynews::app::win10::utils;
+using Platform::Collections::Map;
 using Platform::String;
+using Windows::Foundation::Collections::IMap;
 
 namespace voicemynews {
 namespace tests {
@@ -81,6 +83,59 @@ public:
         String^ output = ConvertStdStrToPlatform(input);
 
         Assert::IsNull(output);
+    }
+
+    TEST_METHOD(ConversionTestsConvertPlatformMapToStd) {
+        auto map = ref new Map<String^, String^>();
+        map->Insert("key1", "Value1");
+        map->Insert("key2", "value2");
+
+        auto result = ConvertPlatformMapToStd(map);
+        
+        Assert::AreEqual(2, static_cast<int>(result.size()));
+        Assert::AreEqual(0, result["key1"].compare(std::string("Value1")));
+        Assert::AreEqual(0, result["key2"].compare(std::string("value2")));
+    }
+
+    TEST_METHOD(ConversionTestsConvertPlatformMapToStdInterfaceOk) {
+        IMap<String^, String^>^ map = ref new Map<String^, String^>();
+        map->Insert("key1", "Value1");
+        map->Insert("key2", "value2");
+
+        auto result = ConvertPlatformMapToStd(map);
+
+        Assert::AreEqual(2, static_cast<int>(result.size()));
+        Assert::AreEqual(0, result["key1"].compare(std::string("Value1")));
+        Assert::AreEqual(0, result["key2"].compare(std::string("value2")));
+    }
+
+    TEST_METHOD(ConversionTestsConvertPlatformMapToStdNullPtr) {
+        Map<String^, String^>^ map = nullptr;
+        auto result = ConvertPlatformMapToStd(map);
+
+        Assert::AreEqual(0, static_cast<int>(result.size()));
+    }
+
+    TEST_METHOD(ConversionTestsConvertStdMapToPlatformOk) {
+        std::map<std::string, std::string> input;
+        input["key1"] = "value1";
+        input["key2"] = "Value2";
+
+        auto output = ConvertStdMapToPlatform(input);
+
+        Assert::IsNotNull(output);
+        Assert::AreEqual(2, static_cast<int>(output->Size));
+        Assert::AreEqual("value1", output->Lookup("key1"));
+        Assert::AreEqual("Value2", output->Lookup("key2"));
+    }
+
+    TEST_METHOD(ConversionTestsConvertStdMapToPlatformUninitialized) {
+        std::map<std::string, std::string> input;
+
+        auto output = ConvertStdMapToPlatform(input);
+
+        Assert::IsNotNull(output);
+        Assert::AreEqual(0, static_cast<int>(output->Size));
     }
 };
 
