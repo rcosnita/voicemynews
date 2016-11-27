8c8bdd72e25b99a38e351fc4663ac591bac6443e
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2016-11-28 00:36:12 +0200

    I added the first version of side menu.

--- src/android/VoiceMyNews/app/src/main/java/com/voicemynews/voicemynews/AppStartActivity.java
+++ src/android/VoiceMyNews/app/src/main/java/com/voicemynews/voicemynews/AppStartActivity.java
@@ -1,11 +1,15 @@
 package com.voicemynews.voicemynews;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.voicemynews.core.bindings.events.EventDataBindingNative;
-import com.voicemynews.core.bindings.events.EventHandler;
 import com.voicemynews.core.bindings.events.EventLoopBindingNative;
 
 /**
@@ -14,14 +18,89 @@ import com.voicemynews.core.bindings.events.EventLoopBindingNative;
  * the user will see his news.
  */
 public class AppStartActivity extends Activity {
+    private DrawerLayout sideMenu = null;
+    private ListView sideMenuList = null;
     private EventLoopBindingNative eventLoop = null;
+    private ActionBarDrawerToggle sideMenuToggle = null;
 
+    /**
+     * Provides the creation logic for the application main activity.
+     * @param savedInstanceState The saved state from a previous run session.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_start);
 
         eventLoop = ((App)getApplicationContext()).getEventLoop();
+
+        initSideMenu();
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    /**
+     * In case the configuration change, update all relevant pieces of UI.
+     * @param newConfig the newly received configuration.
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        sideMenuToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * Provides the logic for correctly syncing the state from a previous running instance.
+     * @param savedInstanceState The previous saved instance.
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        sideMenuToggle.syncState();
+    }
+
+    /**
+     * Provides the logc handling the clicks on the menus.
+     * @param item The item selected from the menu.
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (sideMenuToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        // TODO [rcosnita] implement the app menu in here.
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Initializes the side menu of the application. It lists all items from the business logic.
+     */
+    private void initSideMenu() {
+        final String mTitle = getTitle().toString();
+        final String mDrawerTitle = mTitle;
+        String[] titles = {"menu item 1", "menu item 2"};
+        sideMenu = (DrawerLayout) findViewById(R.id.drawer_layout);
+        sideMenuList = (ListView) findViewById(R.id.left_drawer);
+        sideMenuList.setAdapter(new ArrayAdapter<>(this, R.layout.app_side_menu, R.id.drawer_text, titles));
+        sideMenuToggle = new ActionBarDrawerToggle(this, sideMenu,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+
+        sideMenu.addDrawerListener(sideMenuToggle);
     }
 
     /**
