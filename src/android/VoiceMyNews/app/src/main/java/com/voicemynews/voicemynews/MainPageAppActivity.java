e2a7580187474e644eea261c24c3f9db5bafebad
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2016-12-02 03:48:32 +0200

    I encapsulated the main menu into a separate view.

--- src/android/VoiceMyNews/app/src/main/java/com/voicemynews/voicemynews/MainPageAppActivity.java
+++ src/android/VoiceMyNews/app/src/main/java/com/voicemynews/voicemynews/MainPageAppActivity.java
@@ -1,33 +1,14 @@
 package com.voicemynews.voicemynews;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.voicemynews.core.bindings.events.EventDataBindingNative;
-import com.voicemynews.core.bindings.events.EventHandler;
 import com.voicemynews.core.bindings.events.EventLoopBindingNative;
-import com.voicemynews.voicemynews.models.JsonArrayAdapter;
-import com.voicemynews.voicemynews.models.SideMenuState;
-import com.voicemynews.voicemynews.utils.JsResourcesHelper;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.voicemynews.voicemynews.components.MainMenuView;
 
 /**
  * Provides the foundation activity which can be extended by each app main page activity. It provides
@@ -35,15 +16,27 @@ import java.util.Map;
  * used to communicate with app business logic.
  */
 public abstract class MainPageAppActivity extends Activity {
-    private JsonArrayAdapter menuItemsModel = null;
     private EventLoopBindingNative eventLoop = null;
-    private int layoutId;
+    private MainMenuView mainMenu = null;
     private DrawerLayout sideMenu = null;
-    private ListView sideMenuList = null;
-    private ActionBarDrawerToggle sideMenuToggle = null;
+    private int layoutId;
 
     public MainPageAppActivity(@LayoutRes int layoutId) {
         this.layoutId = layoutId;
+    }
+
+    /**
+     * Provides the logic handling the clicks on the menus.
+     * @param item The item selected from the menu.
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mainMenu.getSideMenuToggle().onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -56,14 +49,6 @@ public abstract class MainPageAppActivity extends Activity {
     }
 
     /**
-     * Retrieves the menu items model for the side menu used during navigation.
-     * @return
-     */
-    protected JsonArrayAdapter getMenuItemsModel() {
-        return menuItemsModel;
-    }
-
-    /**
      * Provides the creation logic for the application main activity.
      * @param savedInstanceState The saved state from a previous run session.
      */
@@ -73,11 +58,14 @@ public abstract class MainPageAppActivity extends Activity {
         setContentView(layoutId);
 
         eventLoop = ((App)getApplicationContext()).getEventLoop();
-
-        initSideMenu();
+        sideMenu = (DrawerLayout) findViewById(R.id.side_menu_layout);
+        mainMenu = (MainMenuView) findViewById(R.id.main_menu);
+        mainMenu.setEventLoop(eventLoop);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
+
+        displaySideMenuIfNecessary();
     }
 
     /**
@@ -97,7 +85,7 @@ public abstract class MainPageAppActivity extends Activity {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        sideMenuToggle.onConfigurationChanged(newConfig);
+        mainMenu.getSideMenuToggle().onConfigurationChanged(newConfig);
     }
 
     /**
@@ -107,171 +95,19 @@ public abstract class MainPageAppActivity extends Activity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        sideMenuToggle.syncState();
-    }
-
-    /**
-     * Provides the logic handling the clicks on the menus.
-     * @param item The item selected from the menu.
-     * @return
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (sideMenuToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Initializes the side menu of the application. It lists all items from the business logic.
-     */
-    private void initSideMenu() {
-        final String mTitle = getTitle().toString();
-        final String mDrawerTitle = mTitle;
-        sideMenu = (DrawerLayout) findViewById(R.id.drawer_layout);
-        sideMenuList = (ListView) findViewById(R.id.side_menu);
-        sideMenuToggle = new ActionBarDrawerToggle(this, sideMenu,
-                R.string.sidemenu_open, R.string.sidemenu_close) {
-
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getActionBar().setTitle(mTitle);
-                ((App)getApplicationContext()).getSideMenuState().setVisible(false);
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getActionBar().setTitle(mDrawerTitle);
-                ((App)getApplicationContext()).getSideMenuState().setVisible(true);
-            }
-        };
-
-        sideMenu.addDrawerListener(sideMenuToggle);
-        wireJsMenu();
-
-        displaySideMenuIfNecessary();
+        mainMenu.getSideMenuToggle().syncState();
     }
 
     /**
      * Provides the logic for displaying / hidding the menu if necessary. It uses the global side
      * menu state in order to take this decision.
      */
-    private void displaySideMenuIfNecessary() {
+    protected void displaySideMenuIfNecessary() {
         boolean isSideMenuVisible = ((App)getApplicationContext()).getSideMenuState().isVisible();
         if (isSideMenuVisible) {
             sideMenu.openDrawer(GravityCompat.START);
         } else {
             sideMenu.closeDrawer(GravityCompat.START);
-        }
-
-    }
-
-    /**
-     * Just a test method for making sure menu loading works as expected.
-     */
-    private void wireJsMenu() {
-        eventLoop.on("app:navigation_menu:loaded", new EventHandler() {
-            @Override
-            public void handleEvent(final EventDataBindingNative evtData) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        displayMenuItems(evtData);
-                    }
-                });
-            }
-        });
-
-        eventLoop.emit("app:navigation_menu:load", EventDataBindingNative.getInstanceNative(""));
-    }
-
-    /**
-     * Display the menu items described by the event received as input.
-     * @param evtData the event describing the menu.
-     */
-    private void displayMenuItems(EventDataBindingNative evtData) {
-        try {
-            JSONArray menuItems = new JSONArray(evtData.getEvtData());
-
-            Map<String, JsonArrayAdapter.PopulateViewAction> itemsResources = new HashMap<>();
-            itemsResources.put("icon", new JsonArrayAdapter.PopulateViewAction() {
-                @Override
-                public void populate(JSONObject item, String jsonKey, View view) {
-                    try {
-                        ImageView iconView = (ImageView) view.findViewById(R.id.sidemenu_item_icon);
-                        iconView.setImageResource(JsResourcesHelper.getIdFromDrawableResourcePath(item.getString(jsonKey), iconView.getContext()));
-                    } catch(Exception ex) {
-                        // TODO [rcosnita] handle exceptions correctly.
-                        System.out.println(ex);
-                    }
-
-                }
-            });
-
-            itemsResources.put("label", new JsonArrayAdapter.PopulateViewAction() {
-                @Override
-                public void populate(JSONObject item, String jsonKey, View view) {
-                    TextView labelView = (TextView) view.findViewById(R.id.sidemenu_item_label);
-
-                    try {
-                        labelView.setText(item.getString(jsonKey));
-                    } catch (JSONException ex) {
-                        // TODO [rcosnita] handle exception correctly and log it.
-                        return;
-                    }
-                }
-            });
-
-            menuItemsModel = new JsonArrayAdapter(this, R.layout.app_side_menu, menuItems,
-                    itemsResources, new JsonArrayAdapter.ItemRenderedAction() {
-                @Override
-                public void handleSelectedItem(int position, JSONObject item, View view) {
-                    SideMenuState state = ((App)getApplicationContext()).getSideMenuState();
-
-                    if (position != state.getSelectedItem()) {
-                        return;
-                    }
-
-                    view.setBackgroundResource(R.color.sideMenuItemSelectedBackground);
-                }
-            });
-            sideMenuList.setAdapter(menuItemsModel);
-            sideMenuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    selectMenuItem(position);
-                }
-            });
-
-            int selectedItem = ((App)getApplicationContext()).getSideMenuState().getSelectedItem();
-            if (selectedItem < 0) {
-                return;
-            }
-
-            sideMenuList.setSelection(selectedItem);
-        } catch(Exception ex) {
-            // TODO [rcosnita] handle exceptions in a coherent way.
-            System.out.println(ex);
-        }
-    }
-
-    /**
-     * Provides the logic for handling items selected from the side menu.
-     * @param position The position of the item selected.
-     */
-    private void selectMenuItem(int position) {
-        JSONObject item = (JSONObject)menuItemsModel.getItem(position);
-        ((App)getApplicationContext()).getSideMenuState().setSelectedItem(position);
-
-        try {
-            String evtName = item.getJSONObject("data").getString("evtName");
-            String evtData = item.toString();
-            eventLoop.emit(evtName, EventDataBindingNative.getInstanceNative(evtData));
-        } catch (JSONException ex) {
-            // TODO [rcosnita] handle and log the exception.
-            System.out.println(ex);
         }
     }
 }
